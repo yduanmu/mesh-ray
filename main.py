061afea8b9adbbf6bc1c682aa0bd35d2cae86cbe
melissa <yaoduanmu3@gmail.com>
2025-04-03 19:22:17 -0400

    a

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import numpy as np
-import trimesh
+import trimesh as tm
 
+def main():
+    while True:
+        print("\n------------------------------------------------------------------")
+        file = str(input("File: "))
+        if file == "quit":
+            break
+        
+        mesh = tm.load('./mesh/' + file)
+        r_orig = str(input("x y z coordinates of ray origin: ")).split(" ")
+        r_dir = str(input("x y z coordinates of ray direction: ")).split(" ")
+        
+        print("------------------------------------------------------------------")
+main()
