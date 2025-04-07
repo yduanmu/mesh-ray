e3d675e9f6945030d7c0979c5fc9c6ed21b6eadc
melissa <yaoduanmu3@gmail.com>
2025-04-07 00:48:44 -0400

    choose_median

--- main.py
+++ main.py
@@ -17,25 +17,38 @@ class K3DTree:
 #if <=9 but >2 points, selects the median out of all of them
 #if 2 points, select the first point
 #if 1 point, just select it.
+#returns the index of the median point
 def choose_median(point_list, dim):
     #select 9 or less points
-    selected_list = np.array
+    selected_list = []
+    point_index = []
     if len(point_list) >= 9:
         for i in range(8):
-            j = random.randint(len(point_list)-1)
-            selected_list[i] = point_list[j]
-            point_list.pop(j)       
-    elif len(point_list) == 2|1:
-        return point_list[0]
+            point_index.append(random.randint(len(point_list)-1))
+            selected_list.append(point_list[point_index[i]])
+            np.delete(point_list, point_index[i])
+    elif len(point_list) == 2 or len(point_list) == 1:
+        return 0
     else:
         selected_list = np.array(point_list)
+        for i in range(len(selected_list)):
+            point_index.append(i)
+    
+    selected_nplist = np.array(selected_list)
 
     #calculate median based on dim
-    sorted_list = selected_list[selected_list[:, dim].argsort()] #dim 0=x, 1=y, 2=z
-    return sorted_list[len(sorted_list)//2]
+    sorted_list = selected_nplist[selected_nplist[:, dim].argsort()] #dim 0=x, 1=y, 2=z
+    median = sorted_list[len(sorted_list)//2]
+    # print("median: ",median)
+
+    #find which index that was (could optimize this with a dictionary but meh, <=9 points should be fast enough)
+    for i in range(len(selected_nplist)):
+        if median[0] == selected_nplist[i][0] and median[1] == selected_nplist[i][1] and median[2] == selected_nplist[i][2]:
+            median_index = point_index[i]
+    return median_index
 
 def make_K3DTree(point_list, depth):
-    axis = depth % 3 # 0=x, 1=y, 2=z
+    axis = depth % 3 #axis 0=x, 1=y, 2=z
 
 class Halfedge:
     def __init__(self, start, end, next=None, twin=None):
@@ -83,6 +96,8 @@ def main():
             break
         mesh = m.Mesh.from_file('./mesh/'+file)
         point_list = np.unique(mesh.vectors.reshape(-1, 3), axis=0) #list of unique points in the mesh
+        # print(point_list)
+        # print(choose_median(point_list, 0))
         parse_halfedges(mesh)
         kdtree = make_K3DTree(point_list, 0)
 
